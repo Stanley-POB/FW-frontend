ae38270f67a8f9b61ea8b26fa9c9814b073980a8
pierreobeau <beauliver@gmail.com>
2022-09-19 19:57:31 +0200

    updated script.js, changed variable name year to currentYear

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch("https://fw-backend.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
-    document.querySelector("#year").textContent = data.year;
+    document.querySelector("#year").textContent = data.currentYear;
   });
